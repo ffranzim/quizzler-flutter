3945024c8707a844041000837410e6122c279c10
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2020-07-12 09:46:57 -0300

    Secao 10 - Aula 98

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,18 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  int indexQuestionList = 0;
+
+  List<bool> anwsers = [false, true, true];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[indexQuestionList],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,6 +73,18 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnwser = anwsers[indexQuestionList];
+
+                if (correctAnwser) {
+                  print('User got it right!');
+                } else {
+                  print('User got it wrong!');
+                }
+
+                setState(() {
+                  indexQuestionList++;
+                });
+
                 //The user picked true.
               },
             ),
@@ -79,12 +103,25 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                bool correctAnwser = anwsers[indexQuestionList];
+
+                if (!correctAnwser) {
+                  print('User got it right!');
+                } else {
+                  print('User got it wrong!');
+                }
+
+                setState(() {
+                  indexQuestionList++;
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+//        TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
