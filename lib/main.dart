35d26de671e1ec534ff1f98869ea6409f6027ede
Fernando Franzim <fernando.franzim@gmail.com>
2020-07-13 09:56:12 -0300

    Add windows and init constructors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -27,15 +28,18 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  List<Question> questionBank = [
+    Question(
+        questionAnswer: false,
+        questionText: "You can lead a cow down stairs but not up stairs."),
+    Question(
+        questionAnswer: true,
+        questionText:
+            "Approximately one quarter of human bones are in the feet."),
+    Question(questionAnswer: true, questionText: "'A slug\'s blood is green.")
   ];
 
   int indexQuestionList = 0;
-
-  List<bool> anwsers = [false, true, true];
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +53,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[indexQuestionList],
+                questionBank[indexQuestionList].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,7 +77,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnwser = anwsers[indexQuestionList];
+                bool correctAnwser =
+                    questionBank[indexQuestionList].questionAnswer;
 
                 if (correctAnwser) {
                   print('User got it right!');
@@ -103,7 +108,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnwser = anwsers[indexQuestionList];
+                bool correctAnwser =
+                    questionBank[indexQuestionList].questionAnswer;
 
                 if (!correctAnwser) {
                   print('User got it right!');
